b16e4a833fce72ac38552c42e00a0a89c1547019
Mukhammadaziz <rustamov977075073@gmail.com>
2024-03-05 15:21:10 +0500

    5-dars

---
+++ main.js
@@ -0,0 +1,40 @@
+// let result = str.length;
+// console.log(result);
+
+// if (str.includes('')) {
+//     console.log('ha oz ichiga oladi');
+// } else {
+//     console.log('bunday harf yoq');
+// }
+
+
+
+// let result = str.toUpperCase();
+// let result = str.charAt(4);
+// let result = str.charAt(5);
+// console.log(result);
+// let result = str.slice(0);
+// let result = str.substring(4);
+// let result = str.substr(0,10)
+// console.log(result);
+
+
+// var str = 'Mukhammadaziz'
+// var result = str.toLowerCase();
+
+// if (str.includes('m')) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// console.log(str);
+
+let ism = prompt('Ism kiriting: ');
+let harf = prompt('Harf kiriting: ');
+
+if (ism.includes(harf)) {
+    alert('Bu harf bor');
+} else {
+    alert('Bu harf mavjud emas');
+}
